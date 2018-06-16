1759a70fe5f8fd090bd3f73327cb42da38b182df
Osusara Kammalawatta <30489601+osusara@users.noreply.github.com>
2018-06-17 00:28:30 +0530

    Added Operations sample

---
+++ #5 Operators/src/Demo.kt
@@ -0,0 +1,15 @@
+fun main(args : Array<String>){
+
+    var number1 : Int = 20
+    var number2 : Int = 10
+
+    var addition = number1 + number2
+    var subtraction = number1 - number2
+    var multiplication = number1 * number2
+    var division = number1/number2
+
+    println("Addition of $number1 and $number2 = $addition")
+    println("Subtraction of $number1 and $number2 = $subtraction")
+    println("Multiplication of $number1 and $number2 = $multiplication")
+    println("Division of $number1 and $number2 = $division")
+}
