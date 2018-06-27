27bf3d3202f2e2dae8a4e64efed28073452d1d81
osusara <osusarakammalawatta@yahoo.com>
2018-06-27 12:16:25 +0530

    Added String to Int

---
+++ #14 String to Integer/src/Demo.kt
@@ -0,0 +1,8 @@
+fun main(args : Array<String>){
+
+    var str : String = "4" // Declare and Assigned String variable.
+    var num = str.toInt() // Cast it to an Int. We use toInt() to cast a String to Int.
+
+    num++ // Add 1 to the Int value.
+    println(num) // Print the value.
+}
