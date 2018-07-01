2dd45a3232b76a3328e5f2b2551cc62cb3f68246
osusara <osusarakammalawatta@yahoo.com>
2018-07-01 14:05:22 +0530

    updated

--- #16 Extension Function/src/Test.kt
+++ #16 Extension Function/src/Test.kt
@@ -1,7 +1,9 @@
 class Test{
 
-    var str : String? = null
+    //Declared a String variable and initialized null value.
+    var str : String? = null //Used String? to handle the NullPoinerException.
 
+    //Create the testStr() function to print "str" value.
     fun testStr(){
         println(str)
     }
