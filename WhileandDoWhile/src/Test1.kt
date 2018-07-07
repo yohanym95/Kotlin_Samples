9c939e4782c49c5b2bc10d7a0ea47aa7f9e75ea0
osusara <osusarakammalawatta@yahoo.com>
2018-07-07 09:20:04 +0530

    u

--- WhileandDoWhile/src/Test1.kt
+++ WhileandDoWhile/src/Test1.kt
@@ -1,11 +1,15 @@
 fun main(args : Array<String>){
+
+    // While loop
     var num : Int = 0
-    while(num<10){ //while like same as java
+    while(num<10){ // while is similar to java while loop
         println(num)
         num++
     }
 
     println()
+
+    Do-While loop
     var num1 : Int = 0
     do{
         println(num1)
