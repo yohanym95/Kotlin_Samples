201470dada0a3ae78a07671a2efa045ab9a9b8cb
osusara <osusarakammalawatta@yahoo.com>
2018-07-07 09:30:56 +0530

    update input an while loop codes

--- InputKotlin/src/inputkot.kt
+++ InputKotlin/src/inputkot.kt
@@ -1,13 +1,19 @@
 package input
 
+// Should import java.util.Scanner or *
 import java.util.*
 
 fun main(args:Array<String>){
-    val reader = Scanner (System.`in`)
+
+    val reader = Scanner (System.`in`) // Creating object from Scanner class
+
     println("ANOTHER WAY IN INPUT OUT KOTLIN")
     println("Enter your Name")
+
+    // Take input
     var Name : String = reader.nextLine()
     println("Enter your age")
+
     var age : Int =reader.nextInt()
 
 
