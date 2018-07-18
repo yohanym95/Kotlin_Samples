aeaecc424f5455bd39ac0c243a02666ef0564a9b
osusara <osusarakammalawatta@yahoo.com>
2018-07-18 18:44:05 +0530

    Added Polymorphism

---
+++ #24 Polymorphism/src/Demo.kt
@@ -0,0 +1,19 @@
+open class Father{ //Parent class
+    open fun think(){
+        println("Thinking...")
+    }
+}
+
+class Son : Father(){ //Child class
+    override fun think(){
+        println("Son is Thinking...")
+    }
+}
+
+fun main(args : Array<String>){
+    // "tom" is a "Father" type variable, and assigns a "son" object
+    var tom : Father = Son()
+    tom.think()
+}
+
+// POLYMORPHISM MEANS ASSIGN A SUBCLASS OBJECT TO A SUPER CLASS VARIABLE.
