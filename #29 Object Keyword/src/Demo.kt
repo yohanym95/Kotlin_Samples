76fa457651171ed4d5924e936ecb0259e4649fc6
osusara <osusarakammalawatta@yahoo.com>
2018-08-06 00:27:11 +0530

    Added Object Keyword Lessosn

---
+++ #29 Object Keyword/src/Demo.kt
@@ -0,0 +1,14 @@
+data class Book(var name : String, var price : Int)
+
+//When we use Object keyword it creates an object. There's only one object of this class could have.
+object BookShelf{
+    var books = arrayListOf<Book>()
+}
+
+fun main(args : Array<String>){
+
+    // So we can call "books" using "BookShelf" object.
+    BookShelf.books.add(Book("JAVA", 600))
+    BookShelf.books.add(Book("PHP", 600))
+    BookShelf.books.add(Book("JS", 600))
+}
