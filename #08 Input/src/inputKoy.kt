42a4cbb14ebe18cc77f81759a242608137027780
osusara <osusarakammalawatta@yahoo.com>
2018-08-24 09:47:05 +0530

    update

--- #08 Input/src/inputKoy.kt
+++ #08 Input/src/inputKoy.kt
@@ -1,5 +1,6 @@
 package input
 
+//This is another way. It's easier.
 fun main(args : Array<String>){
 
     var x = readLine() //readLine() do the BufferedReader's job in the backend.
@@ -7,10 +8,13 @@ fun main(args : Array<String>){
 
     println("Enter Your Name : ")
     var name : String = readLine().toString()
+    println(name)
 
     println("Enter your Age : ")
     var age : Int = readLine()!!.toInt()
+    println(age)
 
     println("Enter Your Age :")
-    var age1 : Int = Integer.valueOf(readLine()) //another way to get input in kotlin
+    var age1 : Int = Integer.valueOf(readLine()) //Another way to get input in kotlin
+    print(age1)
 }
