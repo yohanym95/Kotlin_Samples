46760e940450ac4f40df76f69e87361fcc53bcb3
osusara <osusarakammalawatta@yahoo.com>
2018-08-24 09:43:44 +0530

    update

--- #08 Input/src/inputKoy.kt
+++ #08 Input/src/inputKoy.kt
@@ -2,8 +2,11 @@ package input
 
 fun main(args : Array<String>){
 
+    var x = readLine() //readLine() do the BufferedReader's job in the backend.
+    println(x)
+
     println("Enter Your Name : ")
-    var name :String = readLine().toString()
+    var name : String = readLine().toString()
 
     println("Enter your Age : ")
     var age : Int = readLine()!!.toInt()
