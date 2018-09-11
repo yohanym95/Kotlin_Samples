041ccae929e6814257d1b6d87f37a87c11d89915
osusara <osusarakammalawatta@yahoo.com>
2018-09-11 09:23:36 +0530

    Added List

---
+++ #35 List/src/Demo.kt
@@ -0,0 +1,7 @@
+fun main(args : Array<String>){
+    var value: List<Int> = listOf<Int>(2, 5, 3, 9) //A list of Integers.
+    for( i in value)
+        println(i)
+
+    //There are lots of method which we can use with lists.
+}
