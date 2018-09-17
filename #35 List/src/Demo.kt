605e8fa8497cc77a4aa764badedf00bc2b93456c
osusara <osusarakammalawatta@yahoo.com>
2018-09-17 10:10:13 +0530

    Updated List

--- #35 List/src/Demo.kt
+++ #35 List/src/Demo.kt
@@ -6,4 +6,15 @@ fun main(args : Array<String>){
         println(i)
 
     //There are lots of method which we can use with lists.
+
+
+    //Also we can create List of Objects
+    var aliens : List<Alien> = listOf<Alien>(Alien("Navin",70), Alien("Shweta",80))
+
+    for(alien in aliens){
+	println(alien)
+	println(alien.name)
+    }
 }
+
+data class Alien(var name : String, var points : Int)
